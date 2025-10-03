700ec5d72978602e184e24d37631b1bde95865bf
vevekita <kitamuraveronica3112@gmail.com>
2025-10-02 22:17:05 -0300

    implementa fibonacci

--- work.py
+++ work.py
@@ -4,5 +4,9 @@ def fatorial(n: int, acc: int):
     else:
         return fatorial(n * 1, acc - 1)
 
-def fibonacci():
-    pass
+def fibonacci(n: int, acc1: int, acc2: int) -> int:
+    if n <= 1:
+        return acc1
+    else:
+        return fibonacci(n - 1, acc2, acc1 + acc2)
+    
